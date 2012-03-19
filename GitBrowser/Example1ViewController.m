0327b162efc86995b624078367a1ee04e63dbbcf
Peter Friese <peter@peterfriese.de>
2012-03-19 22:51:37 +0100

    Cleanup.

--- GitBrowser/Example1ViewController.m
+++ GitBrowser/Example1ViewController.m
@@ -15,34 +15,6 @@
 @implementation Example1ViewController
 @synthesize imageView;
 
-- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
-{
-    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
-    if (self) {
-        // Custom initialization
-    }
-    return self;
-}
-
-- (void)viewDidLoad
-{
-    [super viewDidLoad];
-    // Do any additional setup after loading the view from its nib.
-}
-
-- (void)viewDidUnload
-{
-    [self setImageView:nil];
-    [super viewDidUnload];
-    // Release any retained subviews of the main view.
-    // e.g. self.myOutlet = nil;
-}
-
-- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
-{
-    return (interfaceOrientation == UIInterfaceOrientationPortrait);
-}
-
 - (IBAction)forkYou:(id)sender {
     [[RKClient sharedClient] get:@"https://github.com/fluidicon.png" delegate:self];
     
