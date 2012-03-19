f937629a75602d74e55016611107202a9fbf823d
Peter Friese <peter@peterfriese.de>
2012-03-19 21:31:35 +0100

    Initial commit.

---
+++ GitBrowser/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  GitBrowser
+//
+//  Created by Peter Friese on 19.03.12.
+//  Copyright (c) 2012 peterfriese.de. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ GitBrowser/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  GitBrowser
+//
+//  Created by Peter Friese on 19.03.12.
+//  Copyright (c) 2012 peterfriese.de. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
